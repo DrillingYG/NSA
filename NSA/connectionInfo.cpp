585918314bee19cf9817f890a8fbc79e15528e46
cg3980 <cg3980@korea.edu>
2020-02-02 21:00:00 +0900

    connection Info

--- NSA/connectionInfo.cpp
+++ NSA/connectionInfo.cpp
@@ -1,1 +1,44 @@
 #include "connectionInfo.h"
+
+
+void connectionInfo::setConnectionType(void) {
+#define MAX_KEY_NAME_LENGTH 255
+
+	//RegGetValueW(HKEY_CURRENT_USER, L"\Software\Microsoft\Windows\CurrentVersion\Explorer\Map Network Drive MRU");
+	//RegGetValueW(HKEY_CURRENT_USER, L"\Software\Microsoft\Windows\CurrentVersion\Explorer\MountPoints2\##[IP]#[Directory]");
+	//RegGetValueW(HKEY_CURRENT_USER, L"\Network");
+	HKEY hkeyNetwork;
+	DWORD numOfSubkeys;
+	RegOpenKeyExW(HKEY_CURRENT_USER, L"Network", 0, KEY_READ, &hkeyNetwork);
+	RegQueryInfoKey(hkeyNetwork, NULL, 0, 0, &numOfSubkeys, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+	if (numOfSubkeys > 0) {
+		DWORD retCode = 0;
+		DWORD nameLength = MAX_KEY_NAME_LENGTH;
+		wchar_t keyName[MAX_KEY_NAME_LENGTH];
+		for (DWORD i = 0; i < numOfSubkeys; i++) {
+			RegEnumKeyExW(hkeyNetwork, i, keyName, &nameLength, NULL, NULL, NULL, NULL);
+			std::wcout << "subkey : " << keyName << "\n";
+			
+		}
+	}
+	else std::cout << "현재 연결 된 네트워크 장치 없음\n";
+
+	RegCloseKey(hkeyNetwork);
+}
+
+void connectionInfo::printConnectionInfo(void) {
+	switch (storageType) {
+	case NAS:
+
+		break;
+	case Cloud:
+
+		break;
+	case SharedComputer:
+
+		break;
+	default:
+		std::cerr << "Storage type is not defined\n";
+	}
+
+}
--- NSA/connectionInfo.h
+++ NSA/connectionInfo.h
@@ -13,43 +13,9 @@ class connectionInfo
 {
 public:
 
-	void setConnectionType(void) {
-		//RegGetValueW(HKEY_CURRENT_USER, L"\Software\Microsoft\Windows\CurrentVersion\Explorer\Map Network Drive MRU");
-		//RegGetValueW(HKEY_CURRENT_USER, L"\Software\Microsoft\Windows\CurrentVersion\Explorer\MountPoints2\##[IP]#[Directory]");
-		//RegGetValueW(HKEY_CURRENT_USER, L"\Network");
-
-		LONG Result = 0;
-		DWORD index = 0;
-		WCHAR namebuf[256];
-		DWORD buflen = 255;
-		PHKEY hsubkey = NULL;
-
-		while (!Result) {
-			wmemset(namebuf, 0, 256);
-			Result = RegEnumKeyExW(HKEY_CURRENT_USER, index, namebuf, &buflen, NULL, NULL, NULL, NULL);
-			std::cout << "Index : " <<  index << std::endl;;
-			//std::cout << namebuf << std::endl;
-			wprintf(L"namebuf : %s\n", namebuf);
-			std::cout << "key length : " << buflen << std::endl;
-			
-		}
-	}
+	void setConnectionType(void);
 	
-	void printConnectionInfo(void) {
-		switch (storageType) {
-		case NAS:
-
-			break;
-		case Cloud:
-
-			break;
-		case SharedComputer:
-
-			break;
-		default:
-			std::cerr << "Storage type is not defined\n";
-		}
-	}
+	void printConnectionInfo(void);
 	
 private:
 	uint8_t storageType;
