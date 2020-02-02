a4d516ef2470604ec0469ce82553adc65d4cde8b
dfrc <empty7792@gmail.com>
2020-02-02 21:58:30 +0900

    Listing subkeys in connectinInfo.cpp

--- NSA/connectionInfo.cpp
+++ NSA/connectionInfo.cpp
@@ -9,16 +9,19 @@ void connectionInfo::setConnectionType(void) {
 	//RegGetValueW(HKEY_CURRENT_USER, L"\Network");
 	HKEY hkeyNetwork;
 	DWORD numOfSubkeys;
-	RegOpenKeyExW(HKEY_CURRENT_USER, L"Network", 0, KEY_READ, &hkeyNetwork);
+	RegOpenKeyExW(HKEY_CURRENT_USER, L"Network", 0, KEY_READ | KEY_ENUMERATE_SUB_KEYS, &hkeyNetwork);
 	RegQueryInfoKey(hkeyNetwork, NULL, 0, 0, &numOfSubkeys, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 	if (numOfSubkeys > 0) {
 		DWORD retCode = 0;
 		DWORD nameLength = MAX_KEY_NAME_LENGTH;
 		wchar_t keyName[MAX_KEY_NAME_LENGTH];
+		std::cout << "number of subkeys : " << numOfSubkeys << '\n';
 		for (DWORD i = 0; i < numOfSubkeys; i++) {
-			RegEnumKeyExW(hkeyNetwork, i, keyName, &nameLength, NULL, NULL, NULL, NULL);
-			std::wcout << "subkey : " << keyName << "\n";
-			
+			retCode = RegEnumKeyExW(hkeyNetwork, i, keyName, &nameLength, NULL, NULL, NULL, NULL);
+			if (retCode == ERROR_SUCCESS) {
+				std::wcout << i << "th subkey : " << keyName << "\n";
+				nameLength = MAX_KEY_NAME_LENGTH;
+			}			
 		}
 	}
 	else std::cout << "현재 연결 된 네트워크 장치 없음\n";
